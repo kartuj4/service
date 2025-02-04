e2592e1e4e7ed3abbdbf6289b7835c4bdb1a5425
kartuj4 <hrysti.bos@gmail.com>
2025-02-04 09:26:13 +0200

    Update user.h

--- user.h
+++ user.h
@@ -1,1 +1,18 @@
+#ifndef USER_H
+#define USER_H
 
+#include <string>
+
+class User {
+private:
+    std::string name;
+    std::string email;
+    std::string password;
+public:
+    User(std::string n, std::string e, std::string p);
+    std::string getName();
+    std::string getEmail();
+    bool checkPassword(std::string p);
+};
+
+#endif
