20ca09cf68995fadab7562f5b37c975fb6b46f96
kartuj4 <hrysti.bos@gmail.com>
2025-02-04 09:27:20 +0200

    Update ticket.cpp

--- ticket.cpp
+++ ticket.cpp
@@ -1,1 +1,11 @@
+#include "Ticket.h"
 
+Ticket::Ticket(std::string m, std::string s, std::string st) {
+    movie = m;
+    session = s;
+    seat = st;
+}
+
+std::string Ticket::getMovie() { return movie; }
+std::string Ticket::getSession() { return session; }
+std::string Ticket::getSeat() { return seat; }
