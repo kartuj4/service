3bfeada9a346b7310c335ae7d6a6508829d86252
kartuj4 <hrysti.bos@gmail.com>
2025-02-04 09:26:39 +0200

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,1 +1,11 @@
+#include "User.h"
 
+User::User(std::string n, std::string e, std::string p) {
+    name = n;
+    email = e;
+    password = p;
+}
+
+std::string User::getName() { return name; }
+std::string User::getEmail() { return email; }
+bool User::checkPassword(std::string p) { return p == password; }
