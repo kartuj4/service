12ecba09854b6071ce370b0297c37fe9e1a0fcad
kartuj4 <hrysti.bos@gmail.com>
2025-02-04 09:27:32 +0200

    Update database.h

--- database.h
+++ database.h
@@ -1,1 +1,21 @@
+#ifndef DATABASE_H
+#define DATABASE_H
 
+#include "User.h"
+#include "Ticket.h"
+#include <vector>
+
+class Database {
+private:
+    std::vector<User> users;
+    std::vector<Ticket> tickets;
+public:
+    void loadUsers();
+    void saveUsers();
+    void loadTickets();
+    void saveTickets();
+    void addUser(User user);
+    void addTicket(Ticket ticket);
+};
+
+#endif
