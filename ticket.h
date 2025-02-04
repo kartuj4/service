0d4b2c7345313a8b746d66d52808727ee8bba501
kartuj4 <hrysti.bos@gmail.com>
2025-02-04 09:27:05 +0200

    Update ticket.h

--- ticket.h
+++ ticket.h
@@ -1,1 +1,18 @@
+#ifndef TICKET_H
+#define TICKET_H
 
+#include <string>
+
+class Ticket {
+private:
+    std::string movie;
+    std::string session;
+    std::string seat;
+public:
+    Ticket(std::string m, std::string s, std::string seat);
+    std::string getMovie();
+    std::string getSession();
+    std::string getSeat();
+};
+
+#endif
